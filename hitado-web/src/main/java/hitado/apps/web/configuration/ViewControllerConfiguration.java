3269fd90f0438f05a51771f13e7ef5c15d70c662
Erwin Manurung <E.Manurung@allegrodevelopment.com>
2017-11-07 17:24:00 +0700

    init commit

---
+++ hitado-web/src/main/java/hitado/apps/web/configuration/ViewControllerConfiguration.java
@@ -0,0 +1,27 @@
+package hitado.apps.web.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.ResourceBundleViewResolver;
+
+@Configuration
+@ComponentScan({ "hitado.apps.web.controller" })
+public class ViewControllerConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureDefaultServletHandling(
+            DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        ResourceBundleViewResolver viewResolver = new ResourceBundleViewResolver();
+        viewResolver.setBasename("mapping-views");
+        return viewResolver;
+    }
+}
